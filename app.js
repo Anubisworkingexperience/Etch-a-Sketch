db385420a8672283bf2abf8cc0befd65d350c18d
Anubisworkingexperience <79977978+Anubisworkingexperience@users.noreply.github.com>
2023-08-09 00:18:28 +0400

    Add grid structure && some styles

--- app.js
+++ app.js
@@ -4,11 +4,40 @@ const gridSizeText = document.querySelector('.gridSize');
 let gridSize;
 
 range.addEventListener('input', (event) => {
-    gridSizeText.textContent = `${event.target.value} x ${event.target.value}`;
+    gridSizeText.textContent = `Grid size: ${event.target.value} x ${event.target.value}`;
 });
 
 
-function generateGrid() {
-    
+function createGrid() {
+    const grid = document.querySelector('.grid');
+    const rows = 5;
+    const columns = 5;
+
+    grid.style.display = 'flex';
+    grid.style.flexDirection = 'column';
+    grid.style.border = '2px solid black';
+
+    function createElement(){
+    const gridElement = document.createElement('span');
+    gridElement.style.width = '7px';
+    gridElement.style.height = '7px';
+    gridElement.style.backgroundColor = 'white';
+    gridElement.style.border = '2px solid black';
+    gridElement.style.borderRadius = 0;
+    return gridElement;
+    }
+
+   for (i = 0; i < rows; i++) {
+    const elementRow = document.createElement('div');
+    elementRow.style.display = 'flex';
+    for (j = 0; j < columns; j++) {
+        const gridElement = createElement();
+        elementRow.appendChild(gridElement);
+    }
+    grid.appendChild(elementRow);
+   }
+   
 }
+
+createGrid();
 
--- test.js
+++ test.js
@@ -3,6 +3,8 @@ not a part of a project.
 TODO
 1. Grid
 2. Page styling
-3. Background audio
+3. Pen cursor while on grid area
 4. Changing body colors in some time 
+5.Show/hide tools button
 */
+

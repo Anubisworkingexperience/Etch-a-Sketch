dcc27d9ef6fedf66aa90103fb0a9ea28b3f4ef3a
Anubisworkingexperience <79977978+Anubisworkingexperience@users.noreply.github.com>
2023-08-09 18:15:07 +0400

    Add grid

--- app.js
+++ app.js
@@ -3,24 +3,30 @@ const gridSizeText = document.querySelector('.gridSize');
 
 let gridSize;
 
+range.value = 10;
+
 range.addEventListener('input', (event) => {
     gridSizeText.textContent = `Grid size: ${event.target.value} x ${event.target.value}`;
+    removeGrid();
+    createGrid(event.target.value, event.target.value);
 });
 
 
-function createGrid() {
+function createGrid(rows, columns) {
     const grid = document.querySelector('.grid');
-    const rows = 5;
-    const columns = 5;
+    const tools = document.querySelector('.tools');
 
     grid.style.display = 'flex';
     grid.style.flexDirection = 'column';
-    grid.style.border = '2px solid black';
+    grid.style.border = '4px solid red';
+    grid.style.height = '600px';
+    grid.style.width = '600px';
 
     function createElement(){
     const gridElement = document.createElement('span');
-    gridElement.style.width = '7px';
-    gridElement.style.height = '7px';
+    gridElement.style.padding = 0;
+    gridElement.style.width = `${600 / columns}px`;
+    gridElement.style.height = `${600 /rows}px`;
     gridElement.style.backgroundColor = 'white';
     gridElement.style.border = '2px solid black';
     gridElement.style.borderRadius = 0;
@@ -29,7 +35,8 @@ function createGrid() {
 
    for (i = 0; i < rows; i++) {
     const elementRow = document.createElement('div');
-    elementRow.style.display = 'flex';
+    elementRow.classList.add('row');
+    elementRow.style.display = 'flex';;
     for (j = 0; j < columns; j++) {
         const gridElement = createElement();
         elementRow.appendChild(gridElement);
@@ -39,5 +46,14 @@ function createGrid() {
    
 }
 
-createGrid();
+function removeGrid () {
+    const grid = document.querySelector('.grid');
+    const rows = document.querySelectorAll('.row');
+    for (child of rows) {
+        grid.removeChild(child);
+    }
+    
+}
+
+createGrid(10, 10);
 
